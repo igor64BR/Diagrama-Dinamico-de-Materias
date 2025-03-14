693d2787d6e4860789b92d67f79f5bb87bd943a7
baiocco <igor64br@gmail.com>
2025-03-14 17:14:10 -0300

    refactor: atualização no backgroundColor do canva

--- src/pages/reactFlowComponents/FlowBg.tsx
+++ src/pages/reactFlowComponents/FlowBg.tsx
@@ -4,8 +4,16 @@ import React from "react";
 export default function FlowBg() {
   return (
     <>
-      <Background id={'1'} variant={BackgroundVariant.Dots} gap={10} size={1}/>
-      <Background id={'2'} variant={BackgroundVariant.Dots} gap={100} size={3} offset={[6.5, 6.5]}/>
+      <Background
+        id={'2'}
+        variant={BackgroundVariant.Dots}
+        gap={100}
+        size={3}
+        offset={[6.5, 6.5]}
+        style={{backgroundColor: '#121212'}}
+      />
+      <Background id={'1'} variant={BackgroundVariant.Dots} gap={10} size={1} style={{backgroundColor: '#0000'}}/>
     </>
+
   );
 }
