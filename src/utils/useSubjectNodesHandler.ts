6b83693c5296ac56ce31e81de486877d099e38f0
baiocco <igor64br@gmail.com>
2025-03-14 20:46:01 -0300

    feat: useSubjectNodesHandler.getRequirementsTree e useSubjectNodesHandler.getDependentsTree + refactor para utilizar apenas Nodes invés de Subjects

--- src/utils/useSubjectNodesHandler.ts
+++ src/utils/useSubjectNodesHandler.ts
@@ -1,16 +1,35 @@
-import staticSubjects from "@/_data/subjects";
+import subjects1 from "@/_data/subjects";
 import Subject from "@/_infra/entities/Subject";
 import { Node } from "@xyflow/react";
 import { createNode } from "@/_infra/entities/flowEntitiesHandlers/nodeHandler";
 import SubjectBox from "@/pages/components/SubjectBox";
 
 export default function useSubjectNodesHandler() {
-  const subjects = [...staticSubjects];
+  let nodes: Node<Subject>[] = [];
 
-  const getSubjectNodes = (
-    grouped: { [key: number]: Subject[]; }
+  const initialize = () => {
+    initNodes();
+  }
+
+  const initNodes = () => {
+    const grouped = subjects1
+      .reduce((acc, x) => {
+        if (!acc[x.period ?? -1]) {
+          acc[x.period ?? -1] = [];
+        }
+
+        acc[x.period ?? -1].push(x);
+
+        return acc;
+      }, {} as { [key: number]: Subject[] });
+
+    return createNodes(grouped);
+  }
+
+  const createNodes = (
+    nodesGroupedByPeriod: { [key: number]: Subject[]; }
   ) => {
-    const nodes: Node<Subject>[] = [];
+    const createdNodes: Node<Subject>[] = [];
 
     const xInit = 0;
     const yInit = 0;
@@ -23,8 +42,8 @@ export default function useSubjectNodesHandler() {
     let x = xInit;
     let y = yInit;
 
-    for (const period of Object.keys(grouped)) {
-      for (const subject of grouped[Number(period)]) {
+    for (const period of Object.keys(nodesGroupedByPeriod)) {
+      for (const subject of nodesGroupedByPeriod[Number(period)]) {
         if (y > yLimit) {
           y = yInit;
           x += xStep;
@@ -39,7 +58,7 @@ export default function useSubjectNodesHandler() {
           }
         );
 
-        nodes.push(node);
+        createdNodes.push(node);
 
         y += yStep;
       }
@@ -48,34 +67,59 @@ export default function useSubjectNodesHandler() {
       x += xStep * 1.5;
     }
 
-    return nodes;
+    nodes = createdNodes;
   }
+
+  initialize();
 
   return {
     getAll: function () {
-      const grouped = subjects
-        .reduce((acc, x) => {
-          if (!acc[x.period ?? -1]) {
-            acc[x.period ?? -1] = [];
-          }
-
-          acc[x.period ?? -1].push(x);
-
-          return acc;
-        }, {} as { [key: number]: Subject[] });
-
-      return getSubjectNodes(grouped);
+      return nodes;
     },
-    updateSubjectState: function (subject: Subject) {
-      const index = subjects.findIndex(x => x.code === subject.code);
+    getByCode: function(code: string) {
+      const node = nodes.find(x => x.data.code === code);
 
-      subjects[index] = {
-        ...subjects[index],
-        state: subject.state
-      }
+      if (!node) throw new Error(`Node with code ${code} not found`);
+
+      return node;
     },
     getRequirements: function (subject: Subject) {
-      return this.getAll().filter(x => subject.requirements.includes(x.data.code));
+      return this.getAll()
+        .filter(x => subject.requirements.includes(x.data.code));
+    },
+    getRequirementsTree: function (subject: Subject) {
+      const currentRequirements = this.getRequirements(subject);
+
+      const requirements: Node<Subject>[] = [...currentRequirements];
+
+      for (const requirement of currentRequirements) {
+        requirements.push(...this.getRequirementsTree(requirement.data));
+      }
+
+      return requirements;
+    },
+    getDependentsTree: function (subject: Subject) {
+      const dependents = this.getAll()
+        .filter(x => x.data.requirements.includes(subject.code));
+
+      const dependentsTree: Node<Subject>[] = [...dependents];
+
+      for (const dependent of dependents) {
+        dependentsTree.push(...this.getDependentsTree(dependent.data));
+      }
+
+      return dependentsTree;
+    },
+    updateSubjectState: function (subject: Subject) {
+      const index = nodes.findIndex(x => x.data.code === subject.code);
+
+      nodes[index] = {
+        ...nodes[index],
+        data: {
+          ...nodes[index].data,
+          state: subject.state
+        }
+      }
     },
   }
 }
